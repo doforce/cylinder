ee0ad383cded77f5aa65027e640699c2c98fe2fd
Edgar <spot.8tizl@passinbox.com>
2025-04-24 10:01:14 +0800

    bun & pm2

---
+++ pm2.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apps: [
+    {
+      name: "cylinder",
+      script: "node_modules/next/dist/bin/next",
+      args: "start",
+      interpreter: "/root/.bun/bin/bun",
+    },
+  ],
+}
