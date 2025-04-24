3ec01dffbc30910495c2f9ce3ca0cda7ec654773
Edgar <spot.8tizl@passinbox.com>
2025-04-24 10:16:00 +0800

    specific host and port

--- pm2.config.js
+++ pm2.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: "cylinder",
       script: "node_modules/next/dist/bin/next",
-      args: "start",
+      args: "start -H 127.0.0.1 -p 3010",
       interpreter: "/root/.bun/bin/bun",
     },
   ],
